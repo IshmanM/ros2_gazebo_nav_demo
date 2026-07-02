5d764352be1f6330d893b3c25305dbb56b7dbc1e
IshmanM <ishman.mann7@gmail.com>
2026-07-02 18:25:20 -0400

    simple robot created

---
+++ src/robot/launch/robot.launch.py
@@ -0,0 +1,46 @@
+import os
+
+from launch import LaunchDescription
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    xacro_file = PathJoinSubstitution([
+        FindPackageShare("robot"),
+        "urdf",
+        "robot.urdf.xacro"
+    ])
+
+    rviz_config_file = PathJoinSubstitution([
+        FindPackageShare("robot"),
+        "rviz",
+        "nav_demo.rviz"
+    ])
+
+    robot_description = {
+        "robot_description": Command(["xacro ", xacro_file])
+    }
+
+    return LaunchDescription([
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            parameters=[robot_description],
+            output="screen"
+        ),
+
+        Node(
+            package="joint_state_publisher_gui",
+            executable="joint_state_publisher_gui",
+            output="screen"
+        ),
+
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            arguments=["-d", rviz_config_file],
+            output="screen"
+        ),
+    ])
